d63a0b68c40cf6ce553dfddded75a526220f5ee2
Max Fan <max_fan@max-f-mbp.local>
2018-06-12 09:53:31 -0700

    Ruby Tutorial End Of ch5

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+puts '*****static_pages_controller.rb executes'
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
